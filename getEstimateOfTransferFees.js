bc2747c0e5731964d66acb0a4130e81a5d914506
jennjunod <hello@teachjenntech.com>
2023-01-12 10:14:21 -0700

    transferfees

---
+++ getEstimateOfTransferFees.js
@@ -0,0 +1,22 @@
+// getEstimateOfTransferFees.js
+
+import {
+    AxelarQueryAPI, CHAINS, Environment
+  } from "@axelar-network/axelarjs-sdk"
+  
+  async function main() {
+    const axelarQuery = new AxelarQueryAPI({
+      environment: Environment.TESTNET,
+    })
+  
+    const fee = await axelarQuery.getTransferFee(
+      CHAINS.TESTNET.KUJIRA,
+      CHAINS.TESTNET.AVALANCHE,
+      "uausdc",
+      1000000
+    )
+    console.log(fee)
+    return { fee: { denom: 'uausdc', amount: '150000' } }
+  }
+  
+  main()
